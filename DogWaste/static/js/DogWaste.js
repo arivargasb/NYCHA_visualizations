12a0b4268a297a62b46f3aeb023e21a0c4e7e664
Ariadna <ari.vargasb@gmail.com>
2019-11-12 11:05:29 -0600

    making edits to trash/dog waste/litter graphs

--- DogWaste/static/js/DogWaste.js
+++ DogWaste/static/js/DogWaste.js
@@ -128,78 +128,8 @@
 // };
 
 
-// Control --> pre and post
-// Treatment --> pre 
-
-var trace1 = {
-  x: [
-    ['Pre-intervention'], ['Control']
-  ], 
-  y: [50.5], 
-  name: 'Control', 
-  type: 'bar',
-  marker: {color: 'rgb(190, 200, 209)'}
-};
-
-var trace2 = {
-  x: [
-    ['Pre-intervention'], ['Treatment']
-  ], 
-  y: [48.2], 
-  name: 'Treatment', 
-  type: 'bar',
-  marker: { color: 'rgb(0, 153, 51)'}
- };
-
- var trace3 = {
-  x: [
-    ['Post-intervention'], ['Control']
-  ], 
-  y: [43.2], 
-  name: 'Control', 
-  type: 'bar',
-  marker: {color: 'rgb(190, 200, 209)'}
-};
-
-var trace4 = {
-  x: [
-    ['Post-intervention'], ['Treatment']
-  ], 
-  y: [0], 
-  name: 'Treatment', 
-  type: 'bar',
-  marker: { color: 'rgb(0, 153, 51)'}
- };
-
-var data = [trace1, trace2, trace3, trace4];
-var layout = {
-  showlegend: false,
-  xaxis: {
-    tickson: "boundaries",
-    ticklen: 15
-  },
-  yaxis: {range: [0, 55]},
-  font: {
-      family: 'Tw Cen MT',
-      size: 14},
-  yaxis: {title: 'Dog Waste (average daily observations)'},
-  title: 'Change in Dog Waste Observations',
-  annotations: [
-        {
-          x: 2,
-          y: 43,
-          xref: 'x',
-          yref: 'y',
-          text: '-7.3 observations',
-          showarrow: true,
-          arrowhead: 1,
-          ax: 0,
-          ay: -40
-        }]
-};
-
-
-// // Final graph
+// // Control --> pre and post
+// // Treatment --> pre 
 
 // var trace1 = {
 //   x: [
@@ -235,7 +165,7 @@ var layout = {
 //   x: [
 //     ['Post-intervention'], ['Treatment']
 //   ], 
-//   y: [31], 
+//   y: [0], 
 //   name: 'Treatment', 
 //   type: 'bar',
 //   marker: { color: 'rgb(0, 153, 51)'}
@@ -265,19 +195,89 @@ var layout = {
 //           arrowhead: 1,
 //           ax: 0,
 //           ay: -40
-//         },
-//         {
-//           x: 3,
-//           y: 31,
-//           xref: 'x',
-//           yref: 'y',
-//           text: '-17 observations',
-//           showarrow: true,
-//           arrowhead: 1,
-//           ax: 0,
-//           ay: -100
 //         }]
 // };
+
+
+// Final graph
+
+var trace1 = {
+  x: [
+    ['Pre-intervention'], ['Control']
+  ], 
+  y: [50.5], 
+  name: 'Control', 
+  type: 'bar',
+  marker: {color: 'rgb(190, 200, 209)'}
+};
+
+var trace2 = {
+  x: [
+    ['Pre-intervention'], ['Treatment']
+  ], 
+  y: [48.2], 
+  name: 'Treatment', 
+  type: 'bar',
+  marker: { color: 'rgb(0, 153, 51)'}
+ };
+
+ var trace3 = {
+  x: [
+    ['Post-intervention'], ['Control']
+  ], 
+  y: [43.2], 
+  name: 'Control', 
+  type: 'bar',
+  marker: {color: 'rgb(190, 200, 209)'}
+};
+
+var trace4 = {
+  x: [
+    ['Post-intervention'], ['Treatment']
+  ], 
+  y: [31], 
+  name: 'Treatment', 
+  type: 'bar',
+  marker: { color: 'rgb(0, 153, 51)'}
+ };
+
+var data = [trace1, trace2, trace3, trace4];
+var layout = {
+  showlegend: false,
+  xaxis: {
+    tickson: "boundaries",
+    ticklen: 15
+  },
+  yaxis: {range: [0, 55]},
+  font: {
+      family: 'Tw Cen MT',
+      size: 14},
+  yaxis: {title: 'Dog Waste (average daily observations)'},
+  title: 'Change in Dog Waste Observations',
+  annotations: [
+        {
+          x: 2,
+          y: 43,
+          xref: 'x',
+          yref: 'y',
+          text: '-7.3 observations',
+          showarrow: true,
+          arrowhead: 1,
+          ax: 0,
+          ay: -40
+        },
+        {
+          x: 3,
+          y: 31,
+          xref: 'x',
+          yref: 'y',
+          text: '-17 observations',
+          showarrow: true,
+          arrowhead: 1,
+          ax: 0,
+          ay: -100
+        }]
+};
 
 
 Plotly.newPlot('dog_waste', data, layout);
--- Trash/static/js/Trash.js
+++ Trash/static/js/Trash.js
@@ -128,83 +128,10 @@
 // };
 
 
-// Control --> pre and post
-// Treatment --> pre 
+// // Control --> pre and post
+// // Treatment --> pre 
 
-var trace1 = {
-    x: [
-      ['Pre-intervention'], ['Control']
-    ], 
-    y: [50.5], 
-    name: 'Control', 
-    type: 'bar',
-    marker: {color: 'rgb(190, 200, 209)'}
-  };
-  
-  var trace2 = {
-    x: [
-      ['Pre-intervention'], ['Treatment']
-    ], 
-    y: [48.2], 
-    name: 'Treatment', 
-    type: 'bar',
-    marker: { color: 'rgb(0, 153, 51)'}
-   };
-  
-   var trace3 = {
-    x: [
-      ['Post-intervention'], ['Control']
-    ], 
-    y: [63.7], 
-    name: 'Control', 
-    type: 'bar',
-    marker: {color: 'rgb(190, 200, 209)'}
-  };
-  
-  var trace4 = {
-    x: [
-      ['Post-intervention'], ['Treatment']
-    ], 
-    y: [0], 
-    name: 'Treatment', 
-    type: 'bar',
-    marker: { color: 'rgb(0, 153, 51)'}
-   };
-  
-  var data = [trace1, trace2, trace3, trace4];
-  var layout = {
-    showlegend: false,
-    xaxis: {
-      tickson: "boundaries",
-      ticklen: 15
-    },
-    font: {
-        family: 'Tw Cen MT',
-        size: 14},
-    yaxis: {range: [0, 64],
-    title: 'Trash (average daily observations)'},     
-    title: 'Change in Trash Observations',
-    annotations: [
-      {
-        x: 2,
-        y: 63,
-        xref: 'x',
-        yref: 'y',
-        text: '13.2 observations',
-        showarrow: true,
-        arrowhead: 1,
-        ax: 0,
-        ay: 50
-      },
-    
-  ]
-          
-  };
-  
-  
-// //   // Final graph
-  
-//   var trace1 = {
+// var trace1 = {
 //     x: [
 //       ['Pre-intervention'], ['Control']
 //     ], 
@@ -238,7 +165,7 @@ var trace1 = {
 //     x: [
 //       ['Post-intervention'], ['Treatment']
 //     ], 
-//     y: [41.3], 
+//     y: [0], 
 //     name: 'Treatment', 
 //     type: 'bar',
 //     marker: { color: 'rgb(0, 153, 51)'}
@@ -254,33 +181,106 @@ var trace1 = {
 //     font: {
 //         family: 'Tw Cen MT',
 //         size: 14},
-// yaxis: {range: [0, 64],
-//     title: 'Trash (average daily observations)'},
+//     yaxis: {range: [0, 64],
+//     title: 'Trash (average daily observations)'},     
 //     title: 'Change in Trash Observations',
 //     annotations: [
-//           {
-//             x: 2,
-//             y: 63,
-//             xref: 'x',
-//             yref: 'y',
-//             text: '13.2 observations',
-//             showarrow: true,
-//             arrowhead: 1,
-//             ax: 0,
-//             ay: 50
-//           },
-//           {
-//             x: 3,
-//             y: 42,
-//             xref: 'x',
-//             yref: 'y',
-//             text: '-6.9 observations',
-//             showarrow: true,
-//             arrowhead: 1,
-//             ax: 0,
-//             ay: -25
-//           }]
+//       {
+//         x: 2,
+//         y: 63,
+//         xref: 'x',
+//         yref: 'y',
+//         text: '13.2 observations',
+//         showarrow: true,
+//         arrowhead: 1,
+//         ax: 0,
+//         ay: 50
+//       },
+    
+//   ]
+          
 //   };
+  
+  
+//   // Final graph
+  
+  var trace1 = {
+    x: [
+      ['Pre-intervention'], ['Control']
+    ], 
+    y: [50.5], 
+    name: 'Control', 
+    type: 'bar',
+    marker: {color: 'rgb(190, 200, 209)'}
+  };
+  
+  var trace2 = {
+    x: [
+      ['Pre-intervention'], ['Treatment']
+    ], 
+    y: [48.2], 
+    name: 'Treatment', 
+    type: 'bar',
+    marker: { color: 'rgb(0, 153, 51)'}
+   };
+  
+   var trace3 = {
+    x: [
+      ['Post-intervention'], ['Control']
+    ], 
+    y: [63.7], 
+    name: 'Control', 
+    type: 'bar',
+    marker: {color: 'rgb(190, 200, 209)'}
+  };
+  
+  var trace4 = {
+    x: [
+      ['Post-intervention'], ['Treatment']
+    ], 
+    y: [41.3], 
+    name: 'Treatment', 
+    type: 'bar',
+    marker: { color: 'rgb(0, 153, 51)'}
+   };
+  
+  var data = [trace1, trace2, trace3, trace4];
+  var layout = {
+    showlegend: false,
+    xaxis: {
+      tickson: "boundaries",
+      ticklen: 15
+    },
+    font: {
+        family: 'Tw Cen MT',
+        size: 14},
+yaxis: {range: [0, 64],
+    title: 'Trash (average daily observations)'},
+    title: 'Change in Trash Observations',
+    annotations: [
+          {
+            x: 2,
+            y: 63,
+            xref: 'x',
+            yref: 'y',
+            text: '13.2 observations',
+            showarrow: true,
+            arrowhead: 1,
+            ax: 0,
+            ay: 50
+          },
+          {
+            x: 3,
+            y: 42,
+            xref: 'x',
+            yref: 'y',
+            text: '-6.9 observations',
+            showarrow: true,
+            arrowhead: 1,
+            ax: 0,
+            ay: -25
+          }]
+  };
   
   
   Plotly.newPlot('trash', data, layout);
